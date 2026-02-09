fc85678534e1ab43e10aed71c50d1af4c00448bf
MAE3715 <67869851+GQP3715@users.noreply.github.com>
2026-02-09 12:14:15 +0300

    Update Top_Ethernet.vhdl

--- Top_Ethernet.vhdl
+++ Top_Ethernet.vhdl
@@ -1,16 +1,18 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+-- No need to use eth_tx_main.all since we use entity work instantiation [cite: 92]
 
 entity Top_Ethernet is
     Port ( 
         clk_100MHz : in  STD_LOGIC; -- Pin V10 
-        phy_tx_clk : in  STD_LOGIC; -- Pin L5 (25MHz from PHY) 
+        phy_tx_clk : in  STD_LOGIC; -- Pin L5 (25MHz from PHY) [cite: 88, 111]
         btn_reset  : in  STD_LOGIC; -- Pin B8 (Active High) 
-        btn_send   : in  STD_LOGIC; -- Pin D9         
+        btn_send   : in  STD_LOGIC; -- Pin D9 [cite: 88, 114]
+        phy_tx_err : out STD_LOGIC := '0'; -- Add this
         -- MII Pins
-        phy_rst_n  : out STD_LOGIC; -- Pin P3 
-        phy_tx_en  : out STD_LOGIC; -- Pin L2 
+        phy_rst_n  : out STD_LOGIC; -- Pin P3 [cite: 88, 89]
+        phy_tx_en  : out STD_LOGIC; -- Pin L2 [cite: 88, 115]
         phy_tx_data: out STD_LOGIC_VECTOR (3 downto 0); -- T1, T2, U1, U2
         
         -- Debug LEDs
@@ -25,7 +27,7 @@ architecture Behavioral of Top_Ethernet is
     signal heartbeat_25   : unsigned(23 downto 0) := (others => '0'); 
     signal heartbeat_100  : unsigned(24 downto 0) := (others => '0');
 begin
-
+--	phy_tx_err <= '0';
     -- 1. POWER-ON RESET (MANAGED BY 100 MHz)
     -- This holds the PHY in reset (Low) for a short time after power-up.
     -- This prevents the "Deadlock" where the PHY and FPGA wait for each other.
@@ -37,9 +39,9 @@ begin
                 phy_rst_n <= '0';
             elsif por_counter < x"FFFFF" then 
                 por_counter <= por_counter + 1;
-                phy_rst_n <= '0'; -- Hold Reset Low 
+                phy_rst_n <= '0'; -- Hold Reset Low [cite: 91, 92]
             else
-                phy_rst_n <= '1'; -- Release Reset (High) 
+                phy_rst_n <= '1'; -- Release Reset (High) [cite: 91]
             end if;
         end if;
     end process;
@@ -69,7 +71,7 @@ begin
     end process;
 
     -- LED Assignments
-    Leds(0) <= tx_en_internal; -- Should blink extremely fast when sending 
+    Leds(0) <= tx_en_internal; -- Should blink extremely fast when sending [cite: 93]
     Leds(1) <= phy_tx_clk;     -- Very dim (25MHz toggle) but confirms clock presence
     Leds(2) <= heartbeat_25(22);  -- Slow blink = PHY is linked and providing clock
     Leds(3) <= heartbeat_100(23); -- Slow blink = FPGA 100MHz is working
